0c7594c922b59eaa09c0f339fa00e5ed756a6469
razorozx <32021080+razorozx@users.noreply.github.com>
2018-01-01 10:09:23 -0800

    Created basic substance and dosage structs

---
+++ Substance Logger/Substance Logger/Substance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substance_Logger
+{
+    struct Substance
+    {
+        // all of these structs will be logged in a file so the user can pick them later on from a dropdown box
+        public string name;
+        List<Substance> interactions;
+
+        #region Constructors
+        // constructors may not be needed since everything is public and could be set regardless of constructors
+
+        Substance(List<Substance> _interact)
+        {
+            interactions = _interact;
+            name = null;
+        }
+        Substance(string _name, List<Substance> _interact)
+        {
+            interactions = _interact;
+            name = _name;
+        }
+        Substance(string _name)
+        {
+            interactions = new List<Substance>();
+            name = _name;
+        }
+
+        #endregion
+    }
+
+    struct Dosage
+    {
+        Substance substance;
+        public long dose;
+        public string doseType;             // ex: grams, ounces, liters, etc.
+
+
+    }
+}
